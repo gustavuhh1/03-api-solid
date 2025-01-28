553c835ea4f2c021b4fdd64c34cf03a853a965f4
gustavuhh1 <gustavomrodrigues11@gmail.com>
2025-01-28 15:00:11 -0300

    adiciona lógica de registro de usuários em um caso de uso separado e implementa hash de senha com bcryptjs

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply} from 'fastify'
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { registerUseCase } from '@/use-cases/register';
 
 
 
@@ -13,13 +13,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply){
 
   const {name, email, password} = registerBodySchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    }
-  })
+  try {
+    await registerUseCase({name, email, password});
+  } catch (err) {
+    return reply.status(409).send({
+      message: err.message
+    })
+  }
 
   return reply.status(201).send();
 }
