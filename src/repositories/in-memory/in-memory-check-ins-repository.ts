b756c3e21a353f6e7ba63247da27cbb7b64d96a7
Gustavo Martins Rodrigues <gustavomrodrigues11@gmail.com>
2025-02-10 13:24:34 -0300

    adiciona verificação para impedir check-ins duplicados no mesmo dia e implementa repositório de academias em memória

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,11 +1,29 @@
 import { CheckIn, Prisma } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
 
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
+  
+  async findByUserIdOnDate(userId: string, date: Date){
+    const startOftheDay = dayjs(date).startOf('date')
+    const endOftheDay = dayjs(date).endOf('date')
 
+    const checkOnSameDate = this.items.find(checkIn => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const isOnSameDate = checkInDate.isAfter(startOftheDay) && checkInDate.isBefore(endOftheDay)
+
+      return checkIn.user_id === userId && isOnSameDate;
+    })
+
+    if(!checkOnSameDate){
+      return null
+    }
+
+    return checkOnSameDate
+  }
   
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
